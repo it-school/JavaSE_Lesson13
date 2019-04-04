c3b6d68e7ae35d955dda333e957399eff81c2c1a
it-school <it@osvita.od.ua>
2019-04-04 19:08:24 +0300

    New minSalary

--- src/com/itschool/Task1/IEmployee.java
+++ src/com/itschool/Task1/IEmployee.java
@@ -1,8 +1,8 @@
 package com.itschool.Task1;
 
 public interface IEmployee {
-    double minimalMonthSalary = 3723;   //  минимальная месячная зарплата в Украине на 01.12.2018
-    double minimalHourSalary = 22.41;   //  минимальная почасовая оплата в Украине на 1.05.2018
+    double minimalMonthSalary = 4123;   //  минимальная месячная зарплата в Украине на 01.1.2019
+    double minimalHourSalary = 24.78;   //  минимальная почасовая оплата в Украине на 01.01.2019
     double typicalsMonthDays = 20.8;    //  среднее кол-во дней в месяце для расчёта зарплаты
     double hoursPerDay = 8;             //  кол-во рабочих часов в день
 
